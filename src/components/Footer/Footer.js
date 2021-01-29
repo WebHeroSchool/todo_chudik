85b37fb7401c8d2475c1818bb5f0c37b388ffebb
Ivan <ivan.burovkin@gmail.com>
2021-01-29 18:51:24 +0400

    lesson18

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (<div>
+    <button >Все</button>
+    <button>Активные</button>
+    <button>Выполненные</button>
+</div>);
+
+export default Footer;
