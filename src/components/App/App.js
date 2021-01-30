84fc4672558d6a6262865bacfa7d9279cae0ad32
Ivan <ivan.burovkin@gmail.com>
2021-01-30 18:37:45 +0400

    applied for the app.js React.Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,30 +5,31 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-/* const todoItem = 'Написать приложение'; */
-
-const App = () => {
-  const items = [
-    {
-      value: 'Написать приложение',
-      isDone: false
-    },
-    {
-      value: 'Прописать props',
-      isDone: true
-    },
-    {
-      value: 'Сдать задание на проверку',
-      isDone: false
-    }
-  ]
-
-  return(
-  <div className={styles.wrap}>
-    <h1>Список дел:</h1>
-    < InputItem />
-    < ItemList items={items}/>
-    < Footer count={3} />
-  </div>);
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Написать приложение',
+        isDone: false
+      },
+      {
+        value: 'Прописать props',
+        isDone: true
+      },
+      {
+        value: 'Сдать задание на проверку',
+        isDone: false
+      }
+    ]
+  
+    return(
+    <div className={styles.wrap}>
+      <h1>Список дел:</h1>
+      < InputItem />
+      < ItemList items={items}/>
+      < Footer count={3} />
+    </div>);
+  }
 }
+
 export default App;
