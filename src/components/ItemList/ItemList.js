e7b7470f4afeb945174f41e0228c10505553bde5
Ivan <ivan.burovkin@gmail.com>
2021-01-30 13:49:05 +0400

    lesson21

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Item from '../Item/Item'
 
-const ItemList = ({ todoItem }) => (<ul>
-    <li>< Item todoItem={todoItem} /></li>
-    <li>< Item todoItem={'Прописать props'} /></li>
-    <li>< Item todoItem={'Сдать задание на проверку'} /></li>
+const ItemList = ({ items }) => (<ul>
+    {items.map(item => <li key={item.value}>< Item value={item.value} /></li>)}
 </ul>);
 
 export default ItemList;
