1ff2ee55a33a366c8665e8962b92c975ad8d6778
Ivan <ivan.burovkin@gmail.com>
2021-02-22 21:04:41 +0400

    requesting data from github

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,74 @@
 import React from 'react';
-
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {Octokit} from '@octokit/rest';
 import styles from './About.module.css';
 
-const About = () => (
-  <div className={styles.wrap}>
-    <h1>Обо мне:</h1>
-  </div>
-  )
+const octokit = new Octokit();
 
-export default About
+class About extends React.Component {
+  state ={
+    isLoading: true,
+    isError: false,
+    error: '',
+    repoList: [],
+    nameUser: [],
+    avatarUser: []
+  }
+
+  componentDidMount() {
+    octokit.repos.listForUser({
+      username: 'IvanBurovkin'
+    }).then(({data}) => {
+      this.setState({
+        repoList: data,
+        isLoading: false
+      })
+    }).catch(err => {
+        this.setState({
+          error: 'Ошибка получения адреса',
+          isError: true,
+          isLoading: false
+        })
+    });
+
+    octokit.users.getByUsername({
+      username: 'IvanBurovkin'
+    }).then((res) => {
+      this.setState({
+        nameUser: res.data.login,
+        avatarUser: res.data.avatar_url,
+        isLoading: false
+      })
+    }).catch(err => {
+        this.setState({
+          error: 'Ошибка получения адреса',
+          isError: true,
+          isLoading: false
+        })
+      })
+  }
+
+  render() {
+    const { isLoading, isError, error, repoList, nameUser, avatarUser } = this.state;
+
+    return (
+      <CardContent className={styles.wrap}>
+        <div className={styles.header}>
+          <img className={styles.avatar} src={avatarUser} alt={avatarUser}/>
+          <h1 className={styles.title}> {isError ? error : nameUser} </h1>
+        </div>
+
+        <h2 className={styles.subtitle}> {isLoading ? <CircularProgress /> : 'Мои репозитории'} </h2>
+        {!isLoading && <ol className={styles.list}>
+          {isError ? error : repoList.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.html_url}> {repo.name} </a>
+            </li>))}
+        </ol>}
+      </CardContent>
+    );
+  }
+}
+
+export default About;
